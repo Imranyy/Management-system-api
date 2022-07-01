db8bc14df0e312dc052d8c83cf7c5a2f46ade233
imranyy <imranmatano6@gmail.com>
2022-07-01 15:23:38 +0300

    updates2

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 //cors
+app.use(cors({origin:'http://localhost:3000'}))
 app.use(cors({origin:' https://fir-site-bb2a5.web.app'}))
 app.use(cors({origin:'https://fir-site-bb2a5.firebaseapp.com'}))
 
