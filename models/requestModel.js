9e0c6f4969c0dd4918c1c1358aa380ea042193ec
imranyy <imranmatano6@gmail.com>
2022-07-25 15:51:05 +0300

    worked on review route

--- models/requestModel.js
+++ models/requestModel.js
@@ -12,7 +12,7 @@ const requestSchema=new Schema({
     },
     email:{
         type:String,
-        require:true
+        unique:true
     },
     password:{
         type:String,
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -4,6 +4,7 @@ const Schema=mongoose.Schema;
 const reviewSchema=new Schema({
     pic:{
         type:String,
+        unique:false,
         require:true
     },
     name:{
