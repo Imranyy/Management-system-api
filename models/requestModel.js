9a5c13ff6ac431374e374ab99b07bea12caac393
imranyy <imranmatano6@gmail.com>
2022-07-25 15:11:09 +0300

    on point

---
+++ models/requestModel.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const requestSchema=new Schema({
+    pic:{
+        type:String,
+        require:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    reason:{
+        type:String,
+        require:true
+    }
+},{
+    timestamps:true
+})
+module.exports=mongoose.model('request',requestSchema);
